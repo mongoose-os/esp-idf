a07b8426053a264106c90dc4172496d9c02dccde
ruslanvaliullin <r.valiullin77@gmail.com>
2017-06-28 22:57:09 +0500

    Place GPIO API in IRAM

--- components/driver/include/driver/rtc_io.h
+++ components/driver/include/driver/rtc_io.h
@@ -37,7 +37,7 @@ typedef enum {
  * @param gpio_num GPIO number
  * @return true if GPIO is valid for RTC GPIO use. false otherwise.
  */
-inline static bool rtc_gpio_is_valid_gpio(gpio_num_t gpio_num)
+inline static bool IRAM_ATTR rtc_gpio_is_valid_gpio(gpio_num_t gpio_num)
 {
     return gpio_num < GPIO_PIN_COUNT
         && rtc_gpio_desc[gpio_num].reg != 0;
