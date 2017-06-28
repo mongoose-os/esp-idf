a887d5f21c30412330cb3e02ee6d06e3268c3194
ruslanvaliullin <r.valiullin77@gmail.com>
2017-06-28 22:57:09 +0500

    Place GPIO API in IRAM

--- components/driver/rtc_module.c
+++ components/driver/rtc_module.c
@@ -297,7 +297,7 @@ esp_err_t rtc_gpio_set_direction(gpio_num_t gpio_num, rtc_gpio_mode_t mode)
     return ESP_OK;
 }
 
-esp_err_t rtc_gpio_pullup_en(gpio_num_t gpio_num)
+esp_err_t IRAM_ATTR rtc_gpio_pullup_en(gpio_num_t gpio_num)
 {
     //this is a digital pad
     if (rtc_gpio_desc[gpio_num].pullup == 0) {
@@ -312,7 +312,7 @@ esp_err_t rtc_gpio_pullup_en(gpio_num_t gpio_num)
     return ESP_OK;
 }
 
-esp_err_t rtc_gpio_pulldown_en(gpio_num_t gpio_num)
+esp_err_t IRAM_ATTR rtc_gpio_pulldown_en(gpio_num_t gpio_num)
 {
     //this is a digital pad
     if (rtc_gpio_desc[gpio_num].pulldown == 0) {
@@ -327,7 +327,7 @@ esp_err_t rtc_gpio_pulldown_en(gpio_num_t gpio_num)
     return ESP_OK;
 }
 
-esp_err_t rtc_gpio_pullup_dis(gpio_num_t gpio_num)
+esp_err_t IRAM_ATTR rtc_gpio_pullup_dis(gpio_num_t gpio_num)
 {
     //this is a digital pad
     if ( rtc_gpio_desc[gpio_num].pullup == 0 ) {
@@ -342,7 +342,7 @@ esp_err_t rtc_gpio_pullup_dis(gpio_num_t gpio_num)
     return ESP_OK;
 }
 
-esp_err_t rtc_gpio_pulldown_dis(gpio_num_t gpio_num)
+esp_err_t IRAM_ATTR rtc_gpio_pulldown_dis(gpio_num_t gpio_num)
 {
     //this is a digital pad
     if (rtc_gpio_desc[gpio_num].pulldown == 0) {
